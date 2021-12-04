318091afd4958b1bec45f66185bf77a8589a84f2
Maulem <49657965+Maulem@users.noreply.github.com>
2021-12-03 22:11:26 -0300

    Forked & modif files added

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# This rep is used by AWS Cloud Project rep : https://github.com/Maulem/AWS-Cloud-Project
-## The files in here are forked and the modified from: https://github.com/raulikeda/tasks
+# tasks
---
+++ tasks/views.py
@@ -0,0 +1,14 @@
+
+from django.db.models import query
+from rest_framework import viewsets
+from .models import Task
+from .serializer import TaskSerializer
+
+
+
+# Create your views here.
+
+
+class TasksViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
